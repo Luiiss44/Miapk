dc4070b0a9a3e039f63aa4b7a942e80e52cc0059
Luis <lmrodriguez2017@gmail.com>
2025-02-10 13:21:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/miapk/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.inicio
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import androidx.appcompat.app.AppCompatActivity
+import com.example.miapk.R
+import com.example.miapk.SecondActivity
+import com.example.miapk.databinding.ActivityMainBinding
+
+
+class       MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var check: CheckBox;
+    private lateinit var butonLogin: Button;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        // setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        check = findViewById(R.id.checkInicio)
+        butonLogin = findViewById(R.id.btnLogin)
+
+        acciones()
+    }
+
+    private fun acciones() {
+
+        check.setOnCheckedChangeListener { _, b ->
+            butonLogin.isEnabled = b
+        }
+
+        butonLogin.setOnClickListener {
+            // cambio de pantalla
+            val intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
