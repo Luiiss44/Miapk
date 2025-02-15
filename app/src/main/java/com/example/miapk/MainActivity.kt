4b3ac2b86e35fb287fe231f6194b0377a89a60cd
Luis <lmrodriguez2017@gmail.com>
2025-02-15 17:33:15 +0100

    Ventana Registro

--- app/src/main/java/com/example/miapk/MainActivity.kt
+++ app/src/main/java/com/example/miapk/MainActivity.kt
@@ -6,37 +6,47 @@ import android.widget.Button
 import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 import com.example.miapk.R
+import com.example.miapk.RegisterActivity
 import com.example.miapk.SecondActivity
 import com.example.miapk.databinding.ActivityMainBinding
 
 
-class       MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var check: CheckBox;
-    private lateinit var butonLogin: Button;
+    private lateinit var check: CheckBox
+    private lateinit var butonLogin: Button
+    // FALTA ESTE
+    private lateinit var butonRegistro: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        // setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
         check = findViewById(R.id.checkInicio)
         butonLogin = findViewById(R.id.btnLogin)
+        // ENLAZAMOS TAMBIÉN btnRegistro
+        butonRegistro = findViewById(R.id.btnRegistro)
 
         acciones()
     }
 
     private fun acciones() {
-
         check.setOnCheckedChangeListener { _, b ->
             butonLogin.isEnabled = b
         }
 
+        // Acción para botón "Inicio"
         butonLogin.setOnClickListener {
-            // cambio de pantalla
-            val intent = Intent(applicationContext, SecondActivity::class.java)
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        // ACCIÓN PARA BOTÓN "REGISTRO"
+        butonRegistro.setOnClickListener {
+            // Aquí abrimos la RegisterActivity
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
     }
